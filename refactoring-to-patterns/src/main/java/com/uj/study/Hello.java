e6e3ebb32010cbc3fda825c483ed3b0c8479bdea
unclejet <unclejet@126.com>
2020-08-21 07:24:33 +0800

    chore(): init refactor-to-patterns gradle project with junit5.

---
+++ refactoring-to-patterns/src/main/java/com/uj/study/Hello.java
@@ -0,0 +1,11 @@
+package com.uj.study;
+
+/**
+ * @author ：unclejet
+ * @date ：Created in 2020/8/21 上午7:22
+ * @description：
+ * @modified By：
+ * @version:
+ */
+public class Hello {
+}
---
+++ refactoring-to-patterns/src/test/java/com/uj/study/HelloTest.java
@@ -0,0 +1,19 @@
+package com.uj.study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author ：unclejet
+ * @date ：Created in 2020/8/21 上午7:22
+ * @description：
+ * @modified By：
+ * @version:
+ */
+class HelloTest {
+    @Test
+    void hello() {
+
+    }
+}

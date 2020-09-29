b5b89aab032651d3f2b1af190940ff8efa758d11
unclejet <unclejet@126.com>
2020-09-30 07:09:57 +0800

    feat(Compose Method): finished.

--- refactoring-to-patterns/src/main/java/com/uj/study/composeMethod/List.java
+++ refactoring-to-patterns/src/main/java/com/uj/study/composeMethod/List.java
@@ -8,21 +8,32 @@ package com.uj.study.composeMethod;
  * @version:
  */
 public class List {
+    public static final int GROWTH_INCREMENT = 10;
     private boolean readOnly;
     private int size;
     private Object[] elements;
 
     public void add(Object element) {
-        if (!readOnly) {
-            int newSize = size + 1;
-            if (newSize > elements.length) {
-                Object[] newElements =
-                        new Object[elements.length + 10];
-                for (int i = 0; i < size; i++)
-                    newElements[i] = elements[i];
-                elements = newElements;
-            }
-            elements[size++] = element;
-        }
+        if (!readOnly)
+            return;
+        if (atCapacity())
+            grow();
+        addElement(element);
+    }
+
+    private void addElement(Object element) {
+        elements[size++] = element;
+    }
+
+    private void grow() {
+        Object[] newElements =
+                new Object[elements.length + GROWTH_INCREMENT];
+        for (int i = 0; i < size; i++)
+            newElements[i] = elements[i];
+        elements = newElements;
+    }
+
+    private boolean atCapacity() {
+        return (size + 1) > elements.length;
     }
 }
